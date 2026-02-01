156f98b2bd01be3626ff419bc22e8612075fd4c8
MrRichar02 <richarmamadi03@gmail.com>
2026-02-01 00:03:33 -0500

    commit

--- autostart.sh
+++ autostart.sh
@@ -1,3 +1,7 @@
 waybar &
+
 swww-daemon &
-dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots
+
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots &
+
+clipse --listen
